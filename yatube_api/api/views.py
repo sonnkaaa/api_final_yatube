b68bdd32f0f2a03d39ab70e8207dbf64f44bb527
sonka <son1@tpu.ru>
2025-03-02 20:07:13 +0300

    фффф

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets, permissions, filters
 from rest_framework.pagination import LimitOffsetPagination
 from posts.models import Post, Group, Comment, Follow
-from .serializers import PostSerializer, GroupSerializer, CommentSerializer, FollowSerializer
+from .serializers import (PostSerializer,
+                          GroupSerializer, CommentSerializer,
+                          FollowSerializer)
 from .permissions import AuthorOrReadOnly, CanSubscribe
 
 
